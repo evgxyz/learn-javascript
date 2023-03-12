77a0607587251b0304ad46d39fb3f9cda76792f6
Evgeniy <evgeniy8086@ya.ru>
2023-03-12 19:53:54 +0300

    Changes to be committed: 	new file:   11-promise/11.8-async-await-3.js 	modified:   t03-network/3.1-fetch-1.js

---
+++ 11-promise/11.8-async-await-3.js
@@ -0,0 +1,13 @@
+
+'use strict'
+
+async function wait() {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    return 10;
+}
+  
+function f() {
+    wait().then((res) => console.log(res));
+}
+
+f();
--- t03-network/3.1-fetch-1.js
+++ t03-network/3.1-fetch-1.js
@@ -1,20 +1,45 @@
 
 'use strict'
 
-async function funct() {
-    let promise = new Promise((resolve, reject) => {
-        //setTimeout(() => resolve("готово!"), 1000);
-        setTimeout(() => reject("ошибка!"), 1000);
+let names = [
+    'iliakan',
+    'no-such-url',
+    'remy',
+];
+
+async function getUsers(names) {
+    let tasks = names.map(name => 
+        fetch( name != 'remy' ? `https://api.github.com/users/${name}` : 'https://ya.ru')
+        .then(
+            response => { 
+                if (response.ok) {
+                    return response.json()
+                        .catch( err => { throw new Error('json error: ' + err); } ); 
+                }
+                else {
+                    throw new Error('http error: ' + response.status);
+                }
+            }
+        )
+    );
+
+    let results = await Promise.allSettled(tasks);
+    return results.map((result, index) => {
+        if (result.status == "fulfilled") {
+            console.log(`fulfilled: ${names[index]}: ${result.value}`);
+            return result.value;
+        } else
+        if (result.status == "rejected") {
+            console.log(`rejected: ${names[index]}: ${result.reason}`);
+            return null;
+        }
     });
-
-    //let res = await promise;
-
-    return 2;
 }
-  
-funct().then( 
-    (val) => console.log('funct.then: val='+val),
-    (err) => console.log('funct.then: err='+err)
-); // 1
 
-console.log('after then');
+getUsers(names)
+    .then(users => { 
+        console.log('---'); 
+        users.forEach((user, index) => {
+            console.log(`${names[index]}: ${user}`);
+        })
+    });
